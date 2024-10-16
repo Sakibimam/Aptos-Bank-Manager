b1771a2ca4ce2ea8ae7d5b35ab335cc0319064b4
Sakib Imam <sakibimam886@gmail.com>
2024-10-16 14:30:00 +0530

    Add TypeScript and Tailwind CSS configuration

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/profile', // Change to the route you want to redirect to
+        permanent: true, // Set to `true` for a 308 permanent redirect, or `false` for a 307 temporary redirect
+      },
+    ];
+  },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // This allows any hostname
+        port: '',
+        pathname: '/**', // This allows any path
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
+}
+
+export default nextConfig
