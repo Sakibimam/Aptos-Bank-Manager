95ed2db596d204e47cf678cfaa08117c32ab0fdd
Sakib Imam <sakibimam886@gmail.com>
2024-10-23 14:15:00 +0530

    Define TypeScript types and application configuration

---
+++ config/Routes.ts
@@ -0,0 +1,37 @@
+import { BarChart, Coins, Image, User } from "lucide-react"
+
+export const APP_PATHS = {
+  AGGPORTFOLIO: "/aggregated-portfolio",
+  PROFILE: "/profile",
+  TOKENS: "/tokens",
+  NFTS: "/collections",
+  APTOSNAMING: "/aptosnamingservice",
+}
+
+export const APP_ROUTES = [
+  {
+    title: "Profile",
+    Icon: User,
+    href: APP_PATHS.PROFILE,
+  },
+  {
+    title: "Agg Portfolio",
+    Icon: BarChart,
+    href: APP_PATHS.AGGPORTFOLIO,
+  },
+  {
+    title: "Tokens",
+    Icon: Coins,
+    href: APP_PATHS.TOKENS,
+  },
+  {
+    title: "NFTS",
+    Icon: Image,
+    href: APP_PATHS.NFTS,
+  },
+  {
+    title: "AptosNamingService",
+    Icon: Image,
+    href: "/aptosnamingservice",
+  },
+]
---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "AptosManager",
+  description: "Debank for Aptos",
+  links: {
+    twitter: "https://x.com/_kavericodes",
+    docs: "https://ui.shadcn.com",
+  },
+}
